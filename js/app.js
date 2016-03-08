b30a7a6b1040bce8fdce2d6995a4b67854411b48
richardsamuellopez <richardsameullopez@gmail.com>
2016-03-07 21:37:02 -0500

    More

--- js/app.js
+++ js/app.js
@@ -3,11 +3,9 @@ var app = angular.module('demo', ['times.tabletop']).controller('DemoCtrl', func
 
   var public_spreadsheet_url = 'https://docs.google.com/spreadsheets/d/1o0qgdnj9YgMSNxm3vpRxRN39fa8xvE45S9PaqdXU-uk/pubhtml';
 
-// $scope.data=
-data.config(function(TabletopProvider)
-{ TabletopProvider.setTabletopOptions({ key: 'https://docs.google.com/spreadsheets/d/1o0qgdnj9YgMSNxm3vpRxRN39fa8xvE45S9PaqdXU-uk/pubhtml',
- // Your key here. simpleSheet: true // Any Tabletop option except 'callback' works.
-}); });
 
 console.log("HELLO");
-});
+}).config(function(TabletopProvider)
+{ TabletopProvider.setTabletopOptions({ key: 'https://docs.google.com/spreadsheets/d/1o0qgdnj9YgMSNxm3vpRxRN39fa8xvE45S9PaqdXU-uk/pubhtml',
+ // Your key here. simpleSheet: true // Any Tabletop option except 'callback' works.
+})}); 
