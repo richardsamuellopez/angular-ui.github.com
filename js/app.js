6427bae769bbcc2b83e94ed52137446e38733a36
richardsamuellopez <richardsameullopez@gmail.com>
2016-03-12 17:27:23 -0500

    more

--- js/app.js
+++ js/app.js
@@ -51,19 +51,21 @@ console.log("PLAYERS: ",players);
 
           $scope.teamPoints = function(team){
             var points = 0;
-            // for(i=1;i++;i<10){
-            //
-            //   points+=$scope.playerPoints(team[i]);
-            // }
-            points+=$scope.playerPoints(team[1]);
-            points+=$scope.playerPoints(team[2]);
-            points+=$scope.playerPoints(team[3]);
-            points+=$scope.playerPoints(team[4]);
-            points+=$scope.playerPoints(team[5]);
-            points+=$scope.playerPoints(team[6]);
-            points+=$scope.playerPoints(team[7]);
-            points+=$scope.playerPoints(team[8]);
-            points+=$scope.playerPoints(team[9]);
+            console.log("FOR LOOP");
+            for(i=1;i++;i<10){
+
+              points+=$scope.playerPoints(team[i]);
+            }
+
+            // points+=$scope.playerPoints(team[1]);
+            // points+=$scope.playerPoints(team[2]);
+            // points+=$scope.playerPoints(team[3]);
+            // points+=$scope.playerPoints(team[4]);
+            // points+=$scope.playerPoints(team[5]);
+            // points+=$scope.playerPoints(team[6]);
+            // points+=$scope.playerPoints(team[7]);
+            // points+=$scope.playerPoints(team[8]);
+            // points+=$scope.playerPoints(team[9]);
             points+=$scope.playerPoints(team.WC1);
             points+=$scope.playerPoints(team.WC2);
             return points;
