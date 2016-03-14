ff182318cc376ce99af7d9502bbd8bf01b6136a9
richardsamuellopez <richardsameullopez@gmail.com>
2016-03-13 23:15:25 -0400

    more

--- js/app.js
+++ js/app.js
@@ -5,8 +5,7 @@ angular
 ])
 .config(function(TabletopProvider){
     TabletopProvider.setTabletopOptions({
-      // key: 'https://docs.google.com/spreadsheets/d/1o0qgdnj9YgMSNxm3vpRxRN39fa8xvE45S9PaqdXU-uk/pubhtml',
-key:'https://docs.google.com/spreadsheets/d/1KICPKqpWs_hBtyEapP4q8ROez__9cOkeae-VyH792p0'
+      key: 'https://docs.google.com/spreadsheets/d/1o0qgdnj9YgMSNxm3vpRxRN39fa8xvE45S9PaqdXU-uk/pubhtml',
     });
   })
 .controller('PoolCtrl', function($scope, Tabletop){
